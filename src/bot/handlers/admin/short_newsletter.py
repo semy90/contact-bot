c89a425e5f7be53df5cd51f9ae2eae4f588690f1
semyon <semenglinskikh@gmail.com>
2024-07-21 22:07:28 +0500

    добавлена короткая рассылка

---
+++ src/bot/handlers/admin/short_newsletter.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+
+from aiogram import Bot, Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import CallbackQuery, InlineKeyboardButton, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from bot.filters import AdminFilter
+from database.gateway import Database
+
+bot = Bot(os.getenv('TOKEN'))
+short_newsletter_router = Router(name=__name__)
+
+
+@short_newsletter_router.message(AdminFilter(), F.text.startswith("/message"))
+async def short_message(message: Message, session_maker: async_sessionmaker):
+    _, *tmp = message.text.split()
+    print(tmp)
+    text = ' '.join(tmp)
+    async with session_maker() as session:
+        base = Database(session)
+        user_ids = await base.get_all_users()
+        for uid in user_ids:
+            await bot.send_message(chat_id=uid, text=text)
+            await asyncio.sleep(0.05)
+
+    await message.answer('Рассылка завершена!')
